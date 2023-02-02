a1a9c74c4428362ed3b84a64535687d56f216421
vandong97 <phanvandong19977@gmail.com>
2023-02-02 17:06:27 +0700

    nop bt

--- case_study/code/task_16_20.sql
+++ case_study/code/task_16_20.sql
@@ -16,26 +16,7 @@ where nv.ma_nhan_vien not in (
 -- chỉ cập nhật những khách hàng đã từng đặt phòng với 
 -- Tổng Tiền thanh toán trong năm 2021 là lớn hơn 10.000.000 VNĐ.
 
-UPDATE khach_hang 
-SET 
-    khach_hang.ma_khach_hang = 1
-WHERE
-    khach_hang.ma_khach_hang IN (SELECT 
-            *
-        FROM
-            (SELECT 
-                kh.ma_khach_hang
-            FROM
-                khach_hang kh
-            JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
-            JOIN dich_vu dv ON hd.ma_dich_vu = dv.ma_dich_vu
-            JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
-            JOIN dich_vu_di_kem dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
-            WHERE
-                YEAR(hd.ngay_lam_hop_dong) = 2021
-                    AND ma_khach_hang = 2
-            GROUP BY hd.ma_khach_hang
-            HAVING SUM(dv.chi_phi_thue + hdct.so_luong * dvdk.gia) > 10000000));
+
 
 -- 18.	Xóa những khách hàng có hợp đồng trước năm 2021 (chú ý ràng buộc giữa các bảng).
 
