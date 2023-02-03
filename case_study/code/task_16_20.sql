e09b43013e578094057b54332ecc71dab49b33d4
vandong97 <phanvandong19977@gmail.com>
2023-02-03 17:15:16 +0700

    nop bt

--- case_study/code/task_16_20.sql
+++ case_study/code/task_16_20.sql
@@ -16,7 +16,30 @@ where nv.ma_nhan_vien not in (
 -- chỉ cập nhật những khách hàng đã từng đặt phòng với 
 -- Tổng Tiền thanh toán trong năm 2021 là lớn hơn 10.000.000 VNĐ.
 
+create view view_tong_tien as
+select kh.ma_khach_hang, lk.ten_loai_khach,
+sum(ifnull(dv.chi_phi_thue,0) + ifnull(hdct.so_luong,0) * ifnull(dvdk.gia,0)) tong_tien
+from khach_hang kh 
+join loai_khach lk on lk.ma_loai_khach = kh.ma_loai_khach
+join hop_dong hd on hd.ma_khach_hang = kh.ma_khach_hang
+join dich_vu dv on dv.ma_dich_vu = hd.ma_dich_vu
+join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong
+join dich_vu_di_kem dvdk on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+where lk.ten_loai_khach = 'Platinium'
+and year(ngay_lam_hop_dong) = 2021
+group by lk.ma_loai_khach, kh.ma_khach_hang;
 
+select * from view_tong_tien;
+
+update khach_hang
+set ma_khach_hang = 1
+where ma_khach_hang = (select view_tong_tien.ma_khach_hang
+from view_tong_tien
+where view_tong_tien.tong_tien > '1000000'
+and ten_khach_hang = 'Platinium');
+
+select kh.*, lk.* from khach_hang kh
+join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach;
 
 -- 18.	Xóa những khách hàng có hợp đồng trước năm 2021 (chú ý ràng buộc giữa các bảng).
 
